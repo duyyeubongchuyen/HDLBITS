bb80afd04f6f5c1b8ac0e76b9958670322cf3efa
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-08-18 18:15:56 +0700

    feat:solution for dff_with_reset_problem-set

---
+++ CIRCUITS/sequential_logic/latches_and_flip-flops/dff_with_reset/module.v
@@ -0,0 +1,33 @@
+module dff_r(
+    input clk,
+    input reset,
+    input d,
+    output reg q
+);
+    always @(posedge clk) begin
+        if(reset) q = 0;
+        else q <= d;
+    end
+    
+endmodule
+
+module top_module (
+    input clk,
+    input reset,
+    input [7:0] d,
+    output [7:0] q
+);
+
+    genvar i;
+    generate
+        for(i = 0; i < 8 ; i = i + 1) begin : dffr_gen
+            dff_r inst (
+                .clk (clk),
+                .reset(reset),
+                .d(d[i]),
+                .q(q[i])
+            );
+        end
+    endgenerate
+
+endmodule
