5d8d793c3cbf21d034b386a175f24864964e9e39
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-08-29 14:40:24 +0700

    feat: solution for create_circuit_from_truth_table_problem_set

---
+++ CIRCUITS/sequential_logic/latches_and_flip-flops/create_circuit_from_truth_table/module.v
@@ -0,0 +1,19 @@
+module top_module (
+    input clk,
+    input j, k,
+    output reg Q
+);
+
+    always @(posedge clk) begin
+        if(j && k) begin
+            Q <= ~Q;
+        end
+        else if (~j && k) begin
+            Q <= 0;
+        end
+        else if (j && ~k) begin
+            Q <= 1;
+        end
+    end
+    
+endmodule 
