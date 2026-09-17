b72ec5ccfd06411cf43cdb78bf1488c40cfe8031
duyyeubongchuyen <nguyenthaiduy0712@gmail.com>
2026-09-17 22:27:34 +0700

    refactor : update true loigc structure in hardware system (storage and logic must not be merged)

--- CIRCUITS/more_circuits/conway's_game_of_life_16x16/module.v
+++ CIRCUITS/more_circuits/conway's_game_of_life_16x16/module.v
@@ -11,7 +11,7 @@ module top_module (
     generate
         for(i = 0; i < 16; i = i + 1) begin : row
             for(j = 0 ; j < 16 ; j = j + 1) begin : col
-                one_bit_module(
+                one_bit_module inst(
                     .load(load),
                     .data(data[i*16 + j]),
                     .clk(clk),
@@ -50,7 +50,7 @@ module one_bit_module(
     output reg q
 );
 
-    wire[2:0] sum =neighbours_8 + neighbours_7 + neighbours_6 + neighbours_5 + neighbours_3 + neighbours_2 + neighbours_1 + neighbours_0;
+    wire[3:0] sum = neighbours_8 + neighbours_7 + neighbours_6 + neighbours_5 + neighbours_3 + neighbours_2 + neighbours_1 + neighbours_0;
 
     always @(posedge clk) begin
         if(load) q <= data;
@@ -61,4 +61,57 @@ module one_bit_module(
         end
     end
 
+endmodule
+
+// BẢN CẢI TIẾN : PHÂN TẦN LƯU TRỮ RIÊNG, LOGIC RIÊNG
+
+module top_module (
+    input clk,
+    input load,
+    input [255:0] data,
+    output [255:0] q
+);
+
+    genvar i, j;
+
+    wire [255:0] q_next;
+
+    generate
+        for(i = 0 ; i < 16 ; i = i + 1) begin : row
+            for(j = 0 ; j < 16 ; j = j + 1) begin : col
+                one_bit_logic inst (
+                    .q_current(q[i*16 + j]),
+                    .neighbours_0(q[((i - 1 + 16)%16)*16 + ((j - 1 + 16) % 16)]),
+                    .neighbours_1(q[((i - 1 + 16) % 16)*16 + j]),
+                    .neighbours_2(q[((i - 1 + 16) % 16)*16 + ((j + 1) % 16)]),
+                    .neighbours_3(q[i*16 + ((j - 1 + 16) % 16)]),
+                    .neighbours_5(q[i*16 + ((j + 1) % 16)]),
+                    .neighbours_6(q[((i + 1) % 16)*16 + ((j - 1 + 16) % 16)]),
+                    .neighbours_7(q[((i + 1) % 16)*16 + j]),
+                    .neighbours_8(q[((i + 1) % 16)*16 + ((j + 1) % 16)]),
+                    .q_next(q_next[i*16+ j])
+                );
+            end
+        end
+    endgenerate
+
+    always @(posedge clk) begin
+        if(load) q <= data;
+        else q <= q_next;
+    end
+
+endmodule
+
+module one_bit_logic (
+    input q_current,
+    input neighbours_0, neighbours_1, neighbours_2,
+    input neighbours_3, neighbours_5,
+    input neighbours_6, neighbours_7, neighbours_8,
+    output q_next
+);
+
+    wire [3:0] sum = {3'b0, neighbours_8} + {3'b0, neighbours_7} + {3'b0, neighbours_6} + {3'b0, neighbours_5} + {3'b0, neighbours_3} + {3'b0, neighbours_2} + {3'b0, neighbours_1} + {3'b0, neighbours_0};
+
+    assign q_next = (sum == 4'd3) || (sum == 4'd2 && q_current);
+    
 endmodule
